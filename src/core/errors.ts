92c4cb085c9261dc691a0bdddafee7c7e580e64c
Any <day0sant@gmail.com>
2026-06-18 14:28:11 -0300

    Update errors.ts

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -4,3 +4,4 @@ export class PortainerAuthError extends Error {}
 export class StackProvisionError extends Error {}
 export class AddonProvisionError extends Error {}
 export class InvalidConfigurationError extends Error {}
+export class DockerAliasNotFoundError extends Error {}
